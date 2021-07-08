efe8ecdbf29e872d9de456be716b1c2a48a79cff
Wolf0605 <mmd0a605@naver.com>
2021-07-08 11:11:19 +0900

    2021/07/06 second_wolfs_janggo3

--- accountapp/models.py
+++ accountapp/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class HelloWorld(models.Model):
     text = models.CharField(max_length=255, null=False)
-    # 마이그래이션을 통해서 db 를 만들고 model 을 통해서 db 의 한줄을 HelloWorld 로 추가 #}
+    # 마이그래이션을 통해서 db 를 만들고 model 을 통해서 db 의 한줄을 HelloWorld 로 추가 #
